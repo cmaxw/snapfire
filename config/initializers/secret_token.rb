ff452f7885ef2207f6c4b165260c484393bae7d7
woody2shoes <chuck@lostjungles.com>
2010-11-10 01:52:06 -0700

    initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snapfire::Application.config.secret_token = '27c78ac9cff0be7b667f193703a19a473f6e1068299cf7b9551ef17f76c965e7c2ee61be1f2351f11ff8accf80ef6c0487c66535351c457f4efd7233546fe132'
